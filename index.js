324f698c14f982a79451f945e0b332043846d310
Manan Gandhi <ardumanan@gmail.com>
2025-08-10 23:06:37 +0530

    Refactor code

--- index.js
+++ index.js
@@ -1,30 +1,6 @@
-import { parseArgs } from 'util';
-import { readConfig } from './utils/config.js';
-import path from 'path';
+import { getConfigPath, readConfig } from './utils/config.js';
 
-const args = process.argv;
-const options = {
-	conf: {
-		type: 'string',
-		short: 'c'
-	}
-};
-const {
-	values: argValues,
-} = parseArgs({ args, options, allowPositionals: true });
-
-var configFilePath;
-
-if (argValues.conf.startsWith(".")) {
-	configFilePath = path.join(process.cwd(), argValues.conf.slice(2, argValues.conf.length));
-}
-else if (argValues.conf.startsWith("/")) {
-	configFilePath = argValues.conf;
-}
-else {
-	configFilePath = path.join(process.cwd(), argValues.conf)
-}
-
+const configFilePath = getConfigPath()
 const config = readConfig(configFilePath)
 
 console.log(config)
--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,43 @@
 import fs from 'fs'
 import { exit } from 'process'
 import YAML from 'yaml'
+import { z } from 'zod'
+import { parseArgs } from 'util';
+import path from 'path';
+
+const configSchema = z.object({
+	services: z.array(z.object({
+		route: z.string(),
+		endpoint: z.url()
+	}))
+})
+
+export const getConfigPath = () => {
+	const args = process.argv;
+	const options = {
+		conf: {
+			type: 'string',
+			short: 'c'
+		}
+	};
+	const {
+		values: argValues,
+	} = parseArgs({ args, options, allowPositionals: true });
+
+	var configFilePath;
+
+	if (argValues.conf.startsWith(".")) {
+		configFilePath = path.join(process.cwd(), argValues.conf.slice(2, argValues.conf.length));
+	}
+	else if (argValues.conf.startsWith("/")) {
+		configFilePath = argValues.conf;
+	}
+	else {
+		configFilePath = path.join(process.cwd(), argValues.conf)
+	}
+
+	return configFilePath;
+}
 
 export const readConfig = (filePath) => {
 	if (filePath === undefined || filePath === null) {
@@ -18,9 +55,9 @@ export const readConfig = (filePath) => {
 	var config;
 
 	try {
-		config = YAML.parse(rawConf)
+		config = configSchema.parse(YAML.parse(rawConf))
 	} catch (e) {
-		console.error("Error in config file")
+		console.error("Error in config file: ", e)
 		exit(1)
 	}
 
